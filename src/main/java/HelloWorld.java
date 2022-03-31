bb04045f8f955fd9c9ef4eb938372f8ff029e382
sathish103 <92373259+sathish103@users.noreply.github.com>
2022-03-31 15:51:18 +0530

    Update HelloWorld.java
    this is mahesh

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World This is Sathish");
+		get("/hello", (req, res) -> "Hello Krishna....");
 	}
 }
